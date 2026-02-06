3f63afab201802509769b782ad761b7549263cbb
Shubham <shubhamkaushik91@gmail.com>
2026-02-07 01:52:52 +0530

    Updated Files

--- user-svc/src/main/java/com/catalogix/user/config/CorsConfig.java
+++ user-svc/src/main/java/com/catalogix/user/config/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.lang.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CorsConfig {
