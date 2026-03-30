31be606ad27ad5b73b64d77bd10e9a2c7b673468
Shubham <shubhamkaushik91@gmail.com>
2026-03-31 00:23:36 +0530

    Updated Files

--- terraform/envs/dev/ecs.tf
+++ terraform/envs/dev/ecs.tf
@@ -7,7 +7,7 @@
 resource "aws_ecs_cluster" "this" {
   name = "${var.project_name}-cluster"
   setting {
-    name = "containerInsights"
+    name  = "containerInsights"
     value = "enabled"
   }
 }
